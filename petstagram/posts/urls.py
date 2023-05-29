0e2c28bdfe1a6dcf0dd92bd886a06b5e538dad94
Raphade <raphael-pennekamp@web.de>
2023-05-29 13:41:04 +0200

    profile

--- petstagram/posts/urls.py
+++ petstagram/posts/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='posts-home'),
     path('about/', views.about, name='posts-about'),
-    path('profile/',views.home, name='profile'), #TODO: create view, move to other urls.py
 ]
