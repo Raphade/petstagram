141592bc25d88084b39a86118caea3bada2776ee
Nico Werner Keller <nico_werner.keller@smail.th-koeln.de>
2023-06-22 12:40:42 +0200

    terraform adjust bash startup scripts

---
+++ terraform/haproxy/haproxy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+apt-get update
+apt-get install -y haproxy
+
+# Konfiguriere HAProxy
+echo "global
+    log /dev/log local0
+    log /dev/log local1 notice
+    chroot /var/lib/haproxy
+    stats socket /run/haproxy/admin.sock mode 660 level admin expose-fd listeners
+    stats timeout 30s
+    user haproxy
+    group haproxy
+    daemon
+
+defaults
+    log     global
+    mode    http
+    option  httplog
+    option  dontlognull
+    timeout connect 5s
+    timeout client 5s
+    timeout server 10s
+
+frontend http-in
+    bind *:8080
+    default_backend backend-servers
+
+backend backend-servers
+    balance roundrobin
+    option forwardfor
+    server webserver1 <webserver1_internal_ip>:8000 check
+    server webserver2 <webserver2_internal_ip>:8000 check
+    server webserver3 <webserver3_internal_ip>:8000 check
+
+" > /etc/haproxy/haproxy.cfg
+
+systemctl restart haproxy
--- terraform/petstagram/startup.sh
+++ terraform/petstagram/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 apt-get update
-apt-get install -y python3-pip python3-dev build-essential libssl-dev libffi-dev
+apt-get install -y python3-pip python3-dev build-essential libssl-dev libffi-dev nginx
+sudo /etc/init.d/nginx start    # start nginx
+python - m pip install https://projects.unbit.it/downloads/uwsgi-lts.tar.gz
 pip3 install virtualenv
 
 git clone https://github.com/pfad/zur/app /opt/petstagram
@@ -8,7 +10,7 @@ cd /opt/petstagram
 
 virtualenv venv
 source venv/bin/activate
-
+pip install django
 pip install -r requirements.txt
 
 # PostgreSQL-Verbindungseinstellungen
@@ -17,5 +19,48 @@ export DB_NAME="database"
 export DB_USER="database-user"
 export DB_PASSWORD="your_password"
 
+# configure uwsgi
+echo "[uwsgi]
+chdir = /opt/petstagram
+module = petstagram.wsgi:application
+home = /opt/petstagram/venv
+master = true
+processes = 5
+socket = /opt/petstagram/mysite.sock
+chmod-socket = 664
+pidfile = /tmp/project-master.pid
+max-requests = 5000
+vacuum = true
+daemonize = /var/log/uwsgi/petstagram.log
+env = LANG=en_US.UTF-8
+" > /etc/uwsgi.d/petstagram_uwsgi.ini
+
+# Configure Nginx
+echo "upstream django {
+    server unix:///opt/petstagram/mysite.sock;
+}
+
+server {
+    listen 80;
+    server_name example.com;
+
+    location / {
+        uwsgi_pass django;
+        include uwsgi_params;
+    }
+
+    location /static/ {
+        alias /opt/petstagram/static/;
+    }
+
+    location /media/ {
+        alias /opt/petstagram/media/;
+    }
+}
+" > /etc/nginx/sites-available/petstagram
+
+# restart nginx
+service nginx restart
+
 # Django-App mit uWSGI starten
-uwsgi --http :8000 --wsgi-file wsgi.py --master --processes 4 --threads 2 --stats :1717
+sudo uwsgi --ini /etc/uwsgi.d/petstagram_uwsgi.ini
